7c5260e1adeb19ca30bb0c5595a76e2e979ce43b
Victor Suarez <jr._5oct1998@hotmail.com>
2017-02-21 11:30:55 -0600

    Figura, Triangulo, Circulo y Rectangulo
    Creacion de la clase Figura, de la cual heredan caracteristicas las
    clases Triangle, Circle y Rectangle; que a su vez tienen caracteristicas
    propias.

---
+++ Ejemplo figuras/Rectangle.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Rectangulo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rectangle extends Figura
+{
+    private float base;
+    private float altura;
+    
+    public Rectangle(float a, float b)
+    {
+        base = a;
+        altura = b;
+    }
+    
+    public void calcularArea()
+    {
+        area = base * altura;
+    }
+}
